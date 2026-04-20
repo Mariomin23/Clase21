0ea6e2b6da5bae0319e722130d5ae69fedc04fd6
Mariomin23 <mario.minuesa@gmail.com>
2026-04-20 12:41:03 +0200

    refactor db connection setup to use mongoose and dotenv

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,3 +1,11 @@
-export function getConnection() {
-  return 'texto de prueba';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+export async function dbConfig() {
+  try {
+    await mongoose.connect(process.env.DB);
+    console.log('Conexión a MongoDB establecida correctamente');
+  } catch (error) {
+    console.error('Error al conectar con MongoDB:', error.message);
+  }
 }
