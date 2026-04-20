cb04f46d9f814c09b5c5162efb6a6ffb015cb5c9
Mariomin23 <mario.minuesa@gmail.com>
2026-04-20 10:43:32 +0200

    save

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,13 +1,8 @@
 import { Router } from "express";
+import { register } from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.post("/register", (req, res) => {
-  res.json({ message: "Ruta de registro funcionando" });
-});
-
-router.post("/login", (req, res) => {
-  res.json({ message: "Ruta de login funcionando" });
-});
+router.post("/register", register);
 
 export default router;
